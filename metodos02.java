7c52e6e679cbc642ba4f8032ac92b128d14cf4e1
JoaoGabeee <136844527+JoaoGabeee@users.noreply.github.com>
2023-08-04 16:11:24 -0300

    Update metodos02.java

--- metodos02.java
+++ metodos02.java
@@ -5,14 +5,14 @@ public class jsdhvf {
 
     public static void main(String[] args) {
         Random aleatorio = new Random();
-        int saldo = aleatorio.nextInt(2600) + 400;
+        int saldo = aleatorio.nextInt(10000) + 1;
         entrada(saldo);
 
     }
 
     static void entrada(int saldo) {
 
-        int controle = Integer.parseInt(JOptionPane.showInputDialog("ola\n para sacar digite: 0 \n para depositar digite: 1 \n para conferir o saldo digite: 2"));
+        int controle = Integer.parseInt(JOptionPane.showInputDialog(" olá!\n para sacar digite: 0 \n para depositar digite: 1 \n para conferir o saldo digite: 2 \n para sair digite: 3"));
 
         switch (controle) {
 
@@ -30,26 +30,33 @@ public class jsdhvf {
 
                 saldo(saldo);
 
-                break;
-            default:
+            case 3:
 
-                JOptionPane.showMessageDialog(null, "não temos essa opção");
-                entrada(saldo);
+                sair();
+                break;
+
+            default:
+                sair();
         }
 
     }
 
+    static void sair() {
+        JOptionPane.showMessageDialog(null, "obrigado pela preferencia, volte sempre!");
+    }
+
     static void saque(int saldo) {
 
-        int valorDesejadoDoSaque = Integer.parseInt(JOptionPane.showInputDialog("quanto voce deseja sacar? \n saldo atual: " + saldo));
+        int valorDesejadoDoSaque = Integer.parseInt(JOptionPane.showInputDialog("quanto voce deseja sacar? \n saldo atual: R$" + saldo));
 
         if (valorDesejadoDoSaque >= saldo) {
             JOptionPane.showMessageDialog(null, "você não tem dinheiro suficiente!");
         } else {
             JOptionPane.showMessageDialog(null, "valor sacado com sucesso!");
+            saldo = saldo - valorDesejadoDoSaque;
         }
 
-        saldo = saldo - valorDesejadoDoSaque;
+
         saldo(saldo);
 
         entrada(saldo);
@@ -57,7 +64,7 @@ public class jsdhvf {
 
     static void deposito(int saldo) {
 
-        int valorDeposito = Integer.parseInt(JOptionPane.showInputDialog("seu saldo atual é de: " + saldo + " \n quanto você deseja depositar?"));
+        int valorDeposito = Integer.parseInt(JOptionPane.showInputDialog("seu saldo atual é de: R$" + saldo + " \n quanto você deseja depositar?"));
 
         saldo = saldo + valorDeposito;
         saldo(saldo);
@@ -66,9 +73,9 @@ public class jsdhvf {
     }
 
     static void saldo(int saldo) {
-        JOptionPane.showMessageDialog(null, "seu saldo atual é de: " + saldo);
+        JOptionPane.showMessageDialog(null, "seu saldo atual é de: R$" + saldo);
 
-
+        entrada(saldo);
     }
 
 
