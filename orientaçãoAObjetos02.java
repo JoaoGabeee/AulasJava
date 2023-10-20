b358694e0b82e255dd044b4f506232fa5a303959
JoaoGabeee <136844527+JoaoGabeee@users.noreply.github.com>
2023-10-20 16:14:43 -0300

    Create orientaçãoAObjetos02.java

---
+++ orientaçãoAObjetos02.java
@@ -0,0 +1,125 @@
+class Veiculo {
+
+    private String modelo;
+
+    private String cor;
+
+    private  int ano;
+
+    private String placa;
+
+    private double valor;
+
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+
+    public String getCor(){
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+
+    public int getAno(){
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+
+    public double getValor(){
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
+
+
+
+
+
+part 2
+
+
+
+
+
+class Veiculo {
+
+    private String modelo;
+
+    private String cor;
+
+    private  int ano;
+
+    private String placa;
+
+    private double valor;
+
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+
+    public String getCor(){
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+
+    public int getAno(){
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+
+    public double getValor(){
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
